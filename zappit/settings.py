940442406b795484421c6e9983b8a667164d435a
ambroseotundo <ambroseotundo9@gmail.com>
2022-08-26 08:23:31 -0400

    remove keys from push

--- zappit/settings.py
+++ zappit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-00xw@s+soh-pg=*##my9w+17ih_5bnb^h53j0h*y01!7dzj8p9'
+SECRET_KEY = os.environ.get('zappit_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
