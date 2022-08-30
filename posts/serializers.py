e1c0ad10e990f0bee33e859ae1bfe036dd1a12e7
ambroseotundo <ambroseotundo9@gmail.com>
2022-08-30 05:57:28 -0400

    add serializers, ability to vote and constraint to prevent double voting

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Vote
 
 class PostSerializer(serializers.ModelSerializer):
+    # read null fields
+    poster = serializers.ReadOnlyField(source = 'poster.username')
+    poster_id = serializers.ReadOnlyField(source = 'poster.id')
     # fields to be associated with the API
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'poster', 'created']
+        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created']
    
-
+# serializer for voting
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ['id']
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,40 @@
+from rest_framework.exceptions import ValidationError
 from django.shortcuts import render
 from posts import serializers
 from rest_framework import generics
-from .models import Post
-from .serializers import PostSerializer
-
-# Create your views here.
+from .models import Post, Vote
+from .serializers import PostSerializer, VoteSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 # class based view to list out info
-# we are a pulling a db using the qeuerset
+# we are a pulling a db using the qeueryset
 # type of serializer to use
 
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+# permission to create this api
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+# create a field without choosing the current api user.
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+
+# UPVOTE VIEW
+class VoteCreate(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user, post=post)
+
+    def perform_create(self, serializer):
+        # check if use has voted or check a particular post user is already is existence
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post:) ')
+
+        serializer.save(voter=self.request.user, post = Post.objects.get(pk=self.kwargs['pk']))
